1814cb331a02a60bc47a47f13c88c7460f5baaa4
Tomoki <lilac.tm.0x1728@live.jp>
2015-07-18 11:38:57 +0900

    add Twitter Client

--- BMLSimulation.java
+++ BMLSimulation.java
@@ -1,5 +1,9 @@
 package bml2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class BMLSimulation {
 
@@ -10,6 +14,8 @@ public class BMLSimulation {
 	static final int stepToObtain = 100_000;
 	// Trials:
 	static final int timesTrial = 20;
+	// Data file name
+	static final String fileName = "SimulationData.csv";
 
 	/**
 	 * 1回のシミュレーションを行なう
@@ -61,12 +67,81 @@ public class BMLSimulation {
 
 
 	/**
+	 * 文字列を表示してファイルに書き込みます。
+	 * @param fw FileWriterオブジェクト
+	 * @param str 文字列
+	 */
+	public static void putFile(FileWriter fw, String str) {
+		String br = System.getProperty("line.separator");
+		System.out.println(str);
+
+		MyTwitter.tweet(str);
+
+		try {
+			fw.write(str + br);
+		} catch (IOException e) {
+			System.out.println("ファイルに書き込めません。");
+			try {
+				fw.close();
+			} catch (IOException e1) {
+			}
+			System.exit(1);
+		}
+	}
+
+
+	/**
 	 * シミュレーションの再現実験
 	 *
-	 *
+	 * 論文図２に示される、異なるPにおける256*256格子上の
+	 * 密度ρに対する平均速度Vのプロットデータを取得します。
 	 *
 	 */
 	public static void main(String[] args) {
 
+		//Twitter Client初期化
+		MyTwitter.initialize();
+
+		int[] taus = {1, 2};
+		double[] Ps = {0, 0.5, 1};
+		int[] ks = new int[20];
+
+		for (int i = 0; i < ks.length; i++) {
+			ks[i] = i + 1;
+		}
+
+		System.out.println("シミュレーションを開始します。");
+
+		// ファイルを開く
+		String curDir = new File(".").getAbsoluteFile().getParent();
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(curDir + "\\" + fileName);
+		} catch (IOException e) {
+			System.out.println("ファイルを開けません。");
+			System.out.println("シミュレーションを終了します。");
+		}
+
+		double v;
+		for (int tau : taus) {
+			putFile(fw, "tau = " + tau);
+			for (double P : Ps) {
+				putFile(fw, "P = " + P);
+				putFile(fw, "[k], [ρ], [V]");
+				for (int k : ks) {
+					v = simulate(k, tau, P);
+					putFile(fw, "" + k + ", "  + (2.0 * k / L) + ", " + v);
+				}
+			}
+		}
+
+		// ファイルを閉じる
+		try {
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("ファイルをクローズできません。");
+		}
+
+		System.out.println("正常終了");
 	}
 }
---
+++ MyTwitter.java
@@ -0,0 +1,60 @@
+package bml2;
+
+import java.util.Scanner;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+
+public class MyTwitter {
+	private static Twitter twitter;
+
+	public static void initialize() {
+		twitter = new TwitterFactory().getInstance();
+
+		String customerKey = "usBrapr6xIbitQyfbMdSpCM8p";
+		String customerSecret = "80xoBvK4tqsbyvdZUw0h5mj43N7txjbiQquL8EllEjzBnf0Yu2";
+		twitter.setOAuthConsumer(customerKey, customerSecret);
+
+		RequestToken requestToken = null;
+		try {
+			requestToken = twitter.getOAuthRequestToken();
+		} catch (IllegalStateException ie) {
+			// リクエストトークンを取得する必要がない
+			if (!twitter.getAuthorization().isEnabled()) {
+				System.out.println("OAuth key is not set");
+				System.exit(1);
+			}
+		} catch (TwitterException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		AccessToken accessToken = null;
+
+		System.out.println("Grant access to your account: ");
+		System.out.println(requestToken.getAuthenticationURL());
+		System.out.println("PIN?: ");
+		String pin = (new Scanner(System.in)).nextLine();
+
+		try {
+			accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+		} catch (TwitterException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
+		System.out.println("Twitter Client 初期化 正常終了");
+	}
+
+	public static void tweet(String str) {
+		try {
+			twitter.updateStatus(str);
+		} catch (TwitterException e) {
+			System.out.println(e.getMessage());
+			System.out.println("ツイートできません");
+		}
+	}
+}
